7ec78694dff71d7ba1803c2a002eae1b1290d415
dev <dev@dev.com>
2023-08-05 17:16:24 +0800

    coding ci hook

--- bin/jkhook.js
+++ bin/jkhook.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path:'.env.dev'});
 const axios = require('axios');
 
 hookUrl = `http://${process.env.JK_HOST}/buildByToken/build?job=${process.env.JK_JOB}&token=${process.env.JK_TOKEN}`;
